a218258dbb12847c04aa07f1ca665bb1f8613d18
eikooezra <63695703+eikooezra@users.noreply.github.com>
2020-06-30 11:07:40 +0700

    updating wrong codes

--- components/InstantOrder/DataPekerjaan.js
+++ components/InstantOrder/DataPekerjaan.js
@@ -1,268 +1,254 @@
-import React, {Component} from 'react'
-import {
-    StyleSheet,
-    KeyboardAvoidingView,
-    TextInput,
-    View,
-    Text,
-    ScrollView,
-    TouchableOpacity
-} from 'react-native'
-import DropDownPicker from 'react-native-dropdown-picker'
-import DrawerLayout from 'react-native-gesture-handler/DrawerLayout'
+    import React, {Component} from 'react'
+    import {
+        StyleSheet,
+        KeyboardAvoidingView,
+        TextInput,
+        View,
+        Text,
+        ScrollView,
+        TouchableOpacity
+    } from 'react-native'
+    import DrawerLayout from 'react-native-gesture-handler/DrawerLayout'
 
-class DataPekerjaan extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            model: '',
-            isModelFilled: false,
-            errorModel: '',
-            prodYear: '',
-            isProdYearFilled: false,
-            price: '',
-            isPriceFilled: false,
-            errorPrice: '',
-            isPressed: true,
-            null: false
+    class DataPekerjaan extends Component{
+        constructor(props){
+            super(props)
+            this.state={
+                job: '',
+                isJobFilled: false,
+                errorJob: '',
+                salary: '',
+                isSalaryFilled: false,
+                errorSalary: '',
+                exp: '',
+                isExpFilled: false,
+                errorExp: '',
+                isPressed: true,
+                null: false
+            }
         }
-    }
 
-    nullChecker = () => {
-        if(this.state.isPressed === false){
-            this.setState({null: true})
-        } 
-    }
-
-    handleChangeModel = (model) => {
-        this.setState({model})
-        let reg = /^[0-9]*$/
-        if(reg.test(RT) === true){
-            this.setState({isModelFilled: true, errorModel: ''})
-        }else{
-            this.setState({errorModel: 'Tidak boleh kosong'})
+        goToDataPekerjaan = () => {
+            this.props.navigation.navigate('DataMotor')
         }
-    }
 
-    handleChangeProdYear = (prodYear) => {
-        this.setState({prodYear})
-        let reg = /^[0-9]*$/
-        if(reg.test(RT) === true){
-            this.setState({isProdYearFilled: true, errorProdYear: ''})
-        }else{
-            this.setState({errorProdYear: 'Tidak boleh kosong'})
+        nullChecker = () => {
+            if(this.state.isPressed === false){
+                this.setState({null: true})
+            } 
         }
-    }
 
-    handleChangePrice = (price) => {
-        this.setState({price})
-        let reg = /^[0-9]*$/
-        if(reg.test(price) === true){
-            this.setState({isPriceFilled: true, errorPrice: ''})
-        }else {
-            this.setState({errorPrice: 'Tidak boleh kosong'})
+        handleChangeJob = (job) => {
+            this.setState({job})
+            let reg = /^[0-9]*$/
+            if(reg.test(job) === true){
+                this.setState({isJobFilled: true, errorJob: ''})
+            }else{
+                this.setState({errorJob: 'Tidak boleh kosong'})
+            }
         }
-    }
 
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.Header}>
-                    <Text style={styles.txtInstant}>
-                        Instant Order
+        handleChangeSalary = (salary) => {
+            this.setState({salary})
+            let reg = /^[0-9]*$/
+            if(reg.test(salary) === true){
+                this.setState({isSalaryFilled: true, errorSalary: ''})
+            }else{
+                this.setState({errorSalary: 'Tidak boleh kosong'})
+            }
+        }
+
+        handleChangeExp = (exp) => {
+            this.setState({exp})
+            let reg = /^[0-9]*$/
+            if(reg.test(exp) === true){
+                this.setState({isExpFilled: true, errorExp: ''})
+            }else {
+                this.setState({errorExp: 'Tidak boleh kosong'})
+            }
+        }
+
+        render(){
+            return(
+                <View style={styles.container}>
+                    <View style={styles.Header}>
+                        <Text style={styles.txtInstant}>
+                            Instant Order
+                        </Text>
+                    </View>
+
+                    <View style={styles.containerTxt}>
+                        <Text style={styles.txtData}>
+                            Data Pekerjaan
+                        </Text>
+                    </View>
+
+                    <View style={styles.txtInpJob}>
+                        <TextInput
+                            style={styles.txtJob}
+                            placeholder='Pekerjaan'
+                            placeholderTextColor='#7F7F7F'
+                            onChangeText={job => this.handleChangeJob(job)}
+                        />
+                    </View>
+
+                    <Text style={styles.errorStyle}>
+                        {this.state.errorJob}
                     </Text>
-                </View>
 
-                <View style={styles.containerTxt}>
-                    <Text style={styles.txtData}>
-                        Data Pekerjaan
+                    <View style={styles.txtInpSalary}>
+                    <TextInput
+                            style={styles.txtSalary}
+                            placeholder='Penghasilan per bulan'
+                            placeholderTextColor='#7F7F7F'
+                            onChangeText={salary => this.handleChangeSalary(salary)}
+                        />
+                    </View>
+
+                    <Text style={styles.errorStyle}>
+                        {this.state.errorSalary}
                     </Text>
+
+                    <View style={styles.txtInpExp}>
+                        <TextInput
+                            style={styles.txtExp}
+                            placeholder='Lama Bekerja'
+                            placeholderTextColor='#7F7F7F'
+                            onChangeText={exp => this.handleChangeExp(exp)}
+                        />
+                    </View>
+
+                    <Text style={styles.errorStyle}>
+                        {this.state.errorPrice}
+                    </Text>
+
+                    <TouchableOpacity
+                    onPress={this.goToDataMotor}
+                    // disabled={!enabled}
+                >
+                    <Text style={[styles.txtNxt, {
+                        color: (
+                            this.state.job !== '' &&
+                            this.state.salary !== '' &&
+                            this.state.exp !== '')
+                        ? '#0064D0' 
+                        : '#7F7F7F'
+                    }]}>
+                        SELANJUTNYA
+                    </Text>
+                </TouchableOpacity>
                 </View>
-
-                <View style={styles.txtInpModel}>
-                    <TextInput
-                        style={styles.txtModel}
-                        placeholder='Model Motor'
-                        placeholderTextColor='#7F7F7F'
-                        onChangeText={model => this.handleChangeGender(model)}
-                    />
-                </View>
-
-                <Text style={styles.errorStyle}>
-                    {this.state.errorModel}
-                </Text>
-
-                <View>
-                    <DropDownPicker
-                        items={[
-                            {label: '2015', value: '15'},
-                            {label: '2016', value: '16'},
-                            {label: '2017', value: '17'},
-                            {label: '2018', value: '18'},
-                            {label: '2019', value: '19'},
-                            {label: '2020', value: '20'}
-                        ]}
-                        defaultNull={this.nullChecker}
-                        placeholder='Tahun Produksi'
-                        style={{
-                            paddingVertical: 20
-                        }}
-                        containerStyle={{
-                            width: 370,
-                            height: 48,
-                            marginLeft: 16,
-                            marginBottom: 6,
-                        }}
-                        dropDownStyle={{
-                            backgroundColor: '#FFFFFF'
-                        }}
-                        labelStyle={{
-                            marginLeft: 8,
-                            fontSize: 14,
-                            color:'#7F7F7F',
-                            fontFamily: 'Montserrat-SemiBold'
-                        }}
-                        arrowStyle={{
-                            marginLeft: 205
-                        }}
-                        onChangeItem={
-                            item => this.setState({
-                                prodYear: item
-                            })
-                        }
-                        onChangeText={prodYear => this.handleChangeProdYear(prodYear)}
-                    />
-                    />
-                </View>
-
-                <Text style={styles.errorStyle}>
-                    {this.state.errorProdYear}
-                </Text>
-
-                <View style={styles.txtInpPrice}>
-                    <TextInput
-                        style={styles.txtPrice}
-                        placeholder='Harga'
-                        placeholderTextColor='#7F7F7F'
-                        onChangeText={price => this.handleChangePrice(price)}
-                    />
-                </View>
-
-                <Text style={styles.errorStyle}>
-                    {this.state.errorPrice}
-                </Text>
-
-                <TouchableOpacity
-                onPress={this.goToDataPekerjaan}
-                disabled={!enabled}
-            >
-                <Text style={[styles.txtNxt, {
-                    color: (
-                        this.state.model !== '' &&
-                        this.state.prodYear !== '' &&
-                        this.state.price !== '')
-                    ? '#0064D0' 
-                    : '#7F7F7F'
-                }]}>
-                    SELANJUTNYA
-                </Text>
-            </TouchableOpacity>
-            </View>
-        )
+            )
+        }
     }
-}
 
-export default DataPekerjaan
+    export default DataPekerjaan
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF'
-    },
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#FFFFFF'
+        },
 
-    Header: {
-        width: 412,
-        height: 70,
-        flexDirection: 'row',
-        backgroundColor: '#0064D0',
-    },
+        Header: {
+            width: 412,
+            height: 70,
+            flexDirection: 'row',
+            backgroundColor: '#0064D0',
+        },
 
-    txtInstant: {
-        width: 200,
-        height: 20,
-        marginTop: 25,
-        marginLeft: 153,
-        fontSize: 16,
-        color: '#FFFFFF',
-        fontFamily: 'Montserrat-Bold'
-    },
+        txtInstant: {
+            width: 200,
+            height: 20,
+            marginTop: 25,
+            marginLeft: 153,
+            fontSize: 16,
+            color: '#FFFFFF',
+            fontFamily: 'Montserrat-Bold'
+        },
 
-    containerTxt: {
-        width: 420,
-        height: 48,
-        marginBottom: 15,
-        elevation: 4,
-        backgroundColor: '#FFFFFF',
-    },
+        containerTxt: {
+            width: 420,
+            height: 48,
+            marginBottom: 15,
+            elevation: 4,
+            backgroundColor: '#FFFFFF',
+        },
 
-    txtData: {
-        width: 124,
-        height: 19,
-        marginTop: 15,
-        marginLeft: 16,
-        color: '#000000',
-        fontFamily: 'Montserrat-SemiBold'
-    },
+        txtData: {
+            width: 124,
+            height: 19,
+            marginTop: 15,
+            marginLeft: 16,
+            color: '#000000',
+            fontFamily: 'Montserrat-SemiBold'
+        },
 
-    errorStyle: {
-        width: 200,
-        height: 15,
-        marginLeft: 17,
-        marginBottom: 15,
-        fontSize: 12,
-        fontFamily: 'Montserrat-Medium',
-        color: '#D81818'
-    },
+        errorStyle: {
+            width: 200,
+            height: 15,
+            marginLeft: 17,
+            marginBottom: 15,
+            fontSize: 12,
+            fontFamily: 'Montserrat-Medium',
+            color: '#D81818'
+        },
 
-    txtInpModel: {
-        width: 370,
-        height: 48,
-        marginLeft: 16,
-        marginBottom: 6,
-        borderWidth: 2,
-        borderRadius: 4,
-        borderColor: '#EBEBEB',
-    },
+        txtInpJob: {
+            width: 370,
+            height: 48,
+            marginLeft: 16,
+            marginBottom: 6,
+            borderWidth: 2,
+            borderRadius: 4,
+            borderColor: '#EBEBEB',
+        },
 
-    txtModel: {
-        marginLeft: 8,
-        color: '#7F7F7F',
-        fontFamily: 'Montserrat-SemiBold'
-    },
+        txtJob: {
+            marginLeft: 8,
+            color: '#7F7F7F',
+            fontFamily: 'Montserrat-SemiBold'
+        },
 
-    txtInpPrice: {
-        width: 370,
-        height: 48,
-        marginLeft: 16,
-        marginBottom: 6,
-        borderWidth: 2,
-        borderRadius: 4,
-        borderColor: '#EBEBEB',
-    },
+        txtInpSalary: {
+            width: 370,
+            height: 48,
+            marginLeft: 16,
+            marginBottom: 6,
+            borderWidth: 2,
+            borderRadius: 4,
+            borderColor: '#EBEBEB',
+        },
 
-    txtPrice: {
-        marginLeft: 8,
-        color: '#7F7F7F',
-        fontFamily: 'Montserrat-SemiBold'
-    },
+        txtSalary: {
+            marginLeft: 8,
+            color: '#7F7F7F',
+            fontFamily: 'Montserrat-SemiBold'
+        },
 
-    txtNxt: {
-        width: 125,
-        height: 20,
-        marginBottom: 24,
-        fontSize: 16,
-        fontFamily: 'Montserrat-SemiBold',
-        justifyContent: 'center',
-        alignSelf: 'center'
-    },
-})
+        txtInpExp: {
+            width: 370,
+            height: 48,
+            marginLeft: 16,
+            marginBottom: 6,
+            borderWidth: 2,
+            borderRadius: 4,
+            borderColor: '#EBEBEB',
+        },
+
+        txtExp: {
+            marginLeft: 8,
+            color: '#7F7F7F',
+            fontFamily: 'Montserrat-SemiBold'
+        },
+
+        txtNxt: {
+            width: 125,
+            height: 20,
+            marginBottom: 24,
+            fontSize: 16,
+            fontFamily: 'Montserrat-SemiBold',
+            justifyContent: 'center',
+            alignSelf: 'center'
+        },
+    })
