d6fe880078e46a1fd34304a9089398dd062ac7b5
eikooezra <63695703+eikooezra@users.noreply.github.com>
2020-09-09 01:55:27 +0700

    new responsive styling

--- components/Login.js
+++ components/Login.js
@@ -9,6 +9,7 @@ import {
     Image,
 } from 'react-native'
 import axios from 'axios'
+import normalize from 'react-native-normalize';
 import LinearGradient from 'react-native-linear-gradient'
 
 const NODEAPI = 'http://10.3.110.149:3000'
@@ -212,8 +213,7 @@ container: {
 },
 
 split1: {
-    width: 460,
-    height: 320,
+    width: normalize(390),
     backgroundColor: '#1E87F8'
 },
 
@@ -223,7 +223,7 @@ split2: {
 
 txtSandi: {
     color: '#2A77CB',
-    fontSize: 14,
+    fontSize: normalize(14),
     fontFamily: 'Montserrat-SemiBold'
 },
 
@@ -238,13 +238,13 @@ txtMasuk: {
 },
 
 txtError1: {
-    width: 200,
-    height: 18,
-    fontSize: 14,
+    width: normalize(200),
+    height: normalize(18),
+    fontSize: normalize(14),
     fontFamily: 'Monserrat-Medium',
     color: '#E56464',
-    marginBottom: 8,
-    marginLeft: 30
+    marginBottom: normalize(8),
+    marginLeft: normalize(30)
 },
 
 txtError2: {
@@ -258,21 +258,20 @@ txtError2: {
 },
 
 btnMasuk: {
-    width: 200,
-    height: 48,
+    width: normalize(170),
+    height: normalize(46),
     borderRadius: 6,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 16
+    marginBottom: normalize(16)
 },
 
 btnEye: {
-    marginTop: 16,
-    marginBottom: 25,
-    marginLeft: 280,
-    marginRight: 16,
-    width: 25,
-    height: 15.68,
+    marginTop: normalize(16),
+    marginBottom: normalize(25),
+    marginLeft: normalize(260),
+    width: normalize(25),
+    height: normalize(15.68),
     flex: 1,
     position: 'absolute'
 },
@@ -286,12 +285,10 @@ sandiArea: {
 dealerArea: {
     alignSelf: 'center',
     justifyContent: 'center',
-    width: 157.66,
-    height: 23.75,
-    marginTop: 17.47,
-    marginBottom: 92.98,
-    marginLeft: 116,
-    marginRight: 166.34
+    width: normalize(157.66),
+    height: normalize(25.2),
+    marginTop: normalize(17.47),
+    marginBottom: normalize(92.98),
 },
 
 btnArea: {
@@ -300,72 +297,70 @@ btnArea: {
 },
 
 inputContainer1: {
-    marginTop: 36,
-    marginBottom: 8,
+    marginTop: normalize(36),
+    marginBottom: normalize(8),
     borderWidth: 2,
     borderColor: '#000000',
     borderRadius: 6,
-    paddingLeft: 45,
-    marginLeft: 25,
-    marginRight: 25
+    paddingLeft: normalize(45),
+    marginLeft: normalize(25),
+    marginRight: normalize(25)
 },
 
 inputContainer2: {
-    marginBottom: 8,
+    marginBottom: normalize(8),
     borderWidth: 2,
     borderColor: '#000000',
     borderRadius: 6,
-    paddingLeft: 45,
-    marginLeft: 25,
-    marginRight: 25
+    paddingLeft: normalize(45),
+    marginLeft: normalize(25),
+    marginRight: normalize(25)
 },
 
 logo: {
-    width: 168.11,
-    height: 129.09,
-    marginTop: 75,
-    marginLeft: 76.66,
-    marginRight: 117.23,
+    width: normalize(158.11),
+    height: normalize(122),
+    marginTop: normalize(75),
     justifyContent: 'center',
     alignSelf: 'center'
 },
 
 lockLogo: {
-    marginTop: 15,
-    marginBottom: 14.25,
-    marginLeft: 12.69,
-    marginRight: 15.54,
-    width: 13.96,
-    height: 19,
+    marginTop: normalize(15),
+    marginBottom: normalize(14.25),
+    marginLeft: normalize(12.69),
+    marginRight: normalize(15.54),
+    width: normalize(14.5),
+    height: normalize(19),
     borderWidth: 0.8,
     flex: 1,
     position: 'absolute'
 },
 
 mailLogo: {
-    marginTop: 17,
-    marginBottom: 16.29,
-    marginLeft: 10.76,
-    marginRight: 15.54,
-    width: 19,
-    height: 15,
+    marginTop: normalize(17),
+    marginBottom: normalize(16.29),
+    marginLeft: normalize(10.76),
+    marginRight: normalize(15.54),
+    width: normalize(19),
+    height: normalize(15),
     borderWidth: 0.8,
     flex: 1,
     position: 'absolute'
 },
 
 eyeLogo: {
-    width: 25,
-    height: 15.68,
+    width: normalize(25),
+    height: normalize(16.5),
     flex: 1,
     position: 'absolute'
 },
 
 form: {
     width: '100%',
-    height: 468,
-    paddingLeft: 16,
-    paddingRight: 16
+    height: normalize(468),
+    paddingLeft: normalize(16),
+    paddingRight: normalize(16)
 },
 
 error: {
