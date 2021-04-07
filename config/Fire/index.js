e36e4dd87e19009a622360a5335f4549a3f7a303
eikooezra <63695703+eikooezra@users.noreply.github.com>
2021-04-07 15:26:18 +0700

    database url added

--- config/Fire/index.js
+++ config/Fire/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase' 
+// import 'firebase/auth'
+import 'firebase/database'
 
 firebase.initializeApp({
     apiKey: "AIzaSyCV5wZfp8RkKOZ6B0TNAALP50wU4nN5-zE",
@@ -6,6 +8,7 @@ firebase.initializeApp({
     projectId: "momotorid-98c6c",
     storageBucket: "momotorid-98c6c.appspot.com",
     messagingSenderId: "53683180999",
+    databaseURL: `https://momotorid-98c6c-default-rtdb.firebaseio.com/`,
     appId: "1:53683180999:web:c071090a0d81e4485135e2",
     measurementId: "G-NTV5T8DP2M"
   })
