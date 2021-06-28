e8f1aa38cd3e7b18d5b3b80a7be2230adb894fa4
eikooezra <63695703+eikooezra@users.noreply.github.com>
2021-06-29 05:18:54 +0700

    adding new styling, form validation, calc function

--- page/Home/Calculator.js
+++ page/Home/Calculator.js
@@ -15,44 +15,66 @@ class Calculator extends Component {
         this.state={
             price: 0,
             dp: 0,
-            total: 0,
+            total36: 0,
+            total24: 0,
+            total12: 0,
+            isFilled: false
         }
     }
 
     goBack = () => {
         this.props.navigation.navigate('Home')
     }
+
+    isPriceFilled = (price) => {
+        this.setState({price})
+        if(price >= 5000000){
+            this.setState({isFilled: true})
+        }
+    }
+
+    isDpFilled = (dp) => {
+        this.setState({dp})
+        if(dp >= 1500000){
+            this.setState({isFilled: true})
+        }
+    }
     
     calculate = () => {
-        if((this.state.price !=='')&&(this.state.dp !== '')){
+        if((this.state.price !== 0) && (this.state.dp !== 0)){
             let price = this.state.price;
             let dp = this.state.dp;
             let minus = price - dp
             let tenor36 = minus/36;
             this.setState({
-                total:tenor36
+                total36:tenor36
             });
-        }if((this.state.price !=='')&&(this.state.dp !== '')){
+        }if((this.state.price !== 0) && (this.state.dp !== 0)){
             let price = this.state.price;
             let dp = this.state.dp;
             let minus = price - dp
             let tenor24 = minus/24;
             this.setState({
-                total:tenor24
+                total24:tenor24
             });
         }
-        if((this.state.price !=='')&&(this.state.dp !== '')){
+        if((this.state.price !== 0) && (this.state.dp !== 0)){
             let price = this.state.price;
             let dp = this.state.dp;
             let minus = price - dp
             let tenor12 = minus/12;
             this.setState({
-                total:tenor12
+                total12:tenor12
             });
         }
     }
 
         render(){
+
+            const enabled = 
+            this.state.price >= 5000000 &&
+            this.state.dp >= 1500000
+
             return (
                 <View style={styles.container}>
                     <View style={styles.Header}>
@@ -75,29 +97,38 @@ class Calculator extends Component {
                             <Text style={styles.txtPrice}>
                                 Harga
                             </Text>
-        
-                                <View style={styles.txtInpHarga}>
-                                    <TextInput
-                                        style={styles.txtHarga}
-                                        placeholder='Rp 15,000,000'
-                                        value={this.state.price}
-                                        onChangeText={price => this.calculate(price)}
-                                    />
-                                </View>
+
+                            <View style={[styles.txtInpHarga, {
+                                borderColor: (this.state.price >= 5000000)
+                                ? '#0064D0'
+                                : '#B7B7B7'
+                            }]}>
+                                <TextInput
+                                    style={styles.txtHarga}
+                                    placeholder='Rp 15,000,000'
+                                    value={this.state.price}
+                                    onChangeText={price => this.isPriceFilled(price)}
+                                />
                             </View>
                         </View>
-        
-                        <View>
+                    <View>
+                            
                             <Text style={styles.txtDP}>
                                 Uang Muka
                             </Text>
         
-                            <View style={styles.txtInpUangMuka}>
+                            <View style={[styles.txtInpUangMuka,
+                                {
+                                    borderColor: (this.state.dp >= 1500000)
+                                    ? '#0064D0'
+                                    : '#B7B7B7'
+                                }
+                            ]}>
                                 <TextInput
                                     style={styles.txtUangMuka}
                                     placeholder='Rp 1,500,000'
                                     value={this.state.dp}
-                                    onChangeText={dp => this.calculate(dp)}
+                                    onChangeText={dp => this.isDpFilled(dp)}
                                 />
                             </View>
         
@@ -107,8 +138,18 @@ class Calculator extends Component {
                         </View>
         
                         <TouchableOpacity
-                            style={styles.btnCount}
-                            onPress={console.log(form)}
+                            style={[styles.btnCount, 
+                                {
+                                    backgroundColor: (
+                                        this.state.price >= 5000000 &&
+                                        this.state.dp >= 1500000
+                                    )
+                                    ? '#0064D0'
+                                    : '#B7B7B7'
+                                }
+                            ]}
+                            onPress={this.calculate}
+                            disabled={!enabled}
                         >
                             <Text style={styles.txtHitung}>
                                 Hitung
@@ -119,7 +160,60 @@ class Calculator extends Component {
                             *Harga merupakan kisaran, dan dapat berubah,
                             sewaktu-waktu tanpa pemberitahuan
                         </Text>
+
+                        <View style={styles.greenTenorContainer}>
+                            <Text style={styles.txtTenor}>
+                                Tenor
+                            </Text>
+                        </View>
+                        
+                        <View style={styles.whiteTenorContainer}>
+                            <View style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between'
+                                }}>
+                                <Text style={styles.txtLeft}>
+                                    36
+                                </Text>
+
+                                <Text style={styles.txtRight}>
+                                    Rp {this.state.total36}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.greyTenorContainer}>
+                            <View style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between'
+                                }}>
+                                <Text style={styles.txtLeft}>
+                                    24
+                                </Text>
+
+                                <Text style={styles.txtRight}>
+                                    Rp {this.state.total24}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.whiteTenorContainer2}>
+                            <View style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between'
+                                }}>
+                                <Text style={styles.txtLeft}>
+                                    12
+                                </Text>
+
+                                <Text style={styles.txtRight}>
+                                    Rp {this.state.total12}
+                                </Text>
+                            </View>
+                        </View>
+
                     </View>
+                </View>
             )
         }   
     }
@@ -167,56 +261,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFFFFF'
     },
 
-    txtModel: {
-        width: normalize(94),
-        marginTop: normalize(25),
-        marginBottom: normalize(8),
-        marginLeft: normalize(16),
-        fontSize: normalize(14),
-        fontFamily: 'Montserrat-SemiBold'
-    },
-
-    txtInpModel: {
-        width: normalize(320),
-        height: normalize(48),
-        marginLeft: normalize(16),
-        marginBottom: normalize(16),
-        borderRadius: 6,
-        borderWidth: 1
-    },
-
-    txtContoh: {
-        marginLeft: normalize(15),
-        fontFamily: 'Montserrat-Medium'
-    },
-
-    flexContainer: {
-        marginBottom: normalize(14),
-        flexDirection: 'row'
-    },
-
-    txtTahun: {
-        width: normalize(114),
-        marginBottom: normalize(8),
-        marginLeft: normalize(16),
-        fontSize: normalize(14),
-        fontFamily: 'Montserrat-SemiBold'
-    },
-
     txtPrice: {
-        width: normalize(114),
+        marginTop: normalize(15),
         marginBottom: normalize(8),
-        marginLeft: normalize(44),
+        marginLeft: normalize(16),
         fontSize: normalize(14),
         fontFamily: 'Montserrat-SemiBold'
     },
 
     txtInpHarga: {
-        width: normalize(140),
+        width: normalize(320),
         height: normalize(48),
-        marginLeft: normalize(43),
+        marginBottom: normalize(16),
+        marginLeft: normalize(16),
         borderRadius: 6,
         borderWidth: 1,
+        borderColor: '#B7B7B7'
     },
 
     txtHarga: {
@@ -238,7 +298,8 @@ const styles = StyleSheet.create({
         marginLeft: normalize(16),
         marginBottom: normalize(8),
         borderRadius: 6,
-        borderWidth: 1
+        borderWidth: 1,
+        borderColor: '#B7B7B7'
     },
 
     txtUangMuka: {
@@ -254,33 +315,11 @@ const styles = StyleSheet.create({
         fontFamily: 'Montserrat-Medium'
     },
 
-    txtLocation: {
-        width: normalize(84),
-        marginBottom: normalize(8),
-        marginLeft: normalize(16),
-        fontSize: normalize(14),
-        fontFamily: 'Montserrat-SemiBold',
-    },
-
-    txtInpLokasi: {
-        width: normalize(320),
-        height: normalize(48),
-        marginLeft: normalize(16),
-        marginBottom: normalize(35),
-        borderRadius: 6,
-        borderWidth: 1
-    },
-
-    txtLokasi: {
-        marginLeft: normalize(16),
-        fontFamily: 'Montserrat-Medium'
-    },
-
     btnCount: {
         width: normalize(320),
         height: normalize(48),
         marginLeft: normalize(16),
-        marginBottom: normalize(27),
+        marginBottom: normalize(20),
         borderRadius: 6,
         backgroundColor: '#B7B7B7'
     },
@@ -302,6 +341,59 @@ const styles = StyleSheet.create({
         fontSize: normalize(14),
         textAlign: 'center',
         fontFamily: 'Montserrat-Regular'
-    }
+    },
 
+    txtTenor: {
+        marginTop: normalize(15),
+        marginLeft: normalize(16),
+        fontFamily: 'Montserrat-Medium'
+    },
+
+    txtLeft: {
+        marginTop: normalize(15),
+        marginLeft: normalize(28),
+        fontFamily: 'Montserrat-Medium'
+    },
+
+    txtRight: {
+        marginTop: normalize(15),
+        marginRight: normalize(24),
+        fontFamily: 'Montserrat-SemiBold'
+    },
+
+    greenTenorContainer: {
+        width: normalize(320),
+        height: normalize(40),
+        marginLeft: normalize(16),
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
+        elevation: 5,
+        backgroundColor: '#E2F7E3'
+    },
+
+    whiteTenorContainer: {
+        width: normalize(320),
+        height: normalize(48),
+        marginLeft: normalize(16),
+        elevation: 5,
+        backgroundColor: '#FFFFFF'
+    },
+
+    greyTenorContainer: {
+        width: normalize(320),
+        height: normalize(48),
+        marginLeft: normalize(16),
+        elevation: 5,
+        backgroundColor: '#F5F5F5'
+    },
+
+    whiteTenorContainer2: {
+        width: normalize(320),
+        height: normalize(48),
+        marginLeft: normalize(16),
+        borderBottomLeftRadius: 6,
+        borderBottomRightRadius: 6,
+        elevation: 5,
+        backgroundColor: '#FFFFFF'
+    },
 })
